77a91fa01b1040176bf6bb521dc79ab998f10c01
Adam Kunzler <adamkunzler@gmail.com>
2024-03-04 17:11:47 -0600

    add folder for steering library and a demo project

---
+++ Kz.Steering.Demo/Program.cs
@@ -0,0 +1,51 @@
+﻿// https://www.raylib.com/
+// https://github.com/ChrisDill/Raylib-cs
+// dotnet add package Raylib-cs
+
+using Raylib_cs;
+using Color = Raylib_cs.Color;
+
+internal class Program
+{
+    public static void Main()
+    {
+        //
+        // Initialization
+        //        
+        Raylib.InitWindow(1024, 1024, ".: POC :.");
+        Raylib.SetTargetFPS(60);
+
+        //
+        // MAIN RENDER LOOP
+        //
+        while (!Raylib.WindowShouldClose())    // Detect window close button or ESC key
+        {
+            //
+            // PROCESS INPUTS
+            //
+            if (Raylib.IsKeyPressed(KeyboardKey.Space))
+            {
+                // do something...
+            }
+
+            //
+            // UPDATE STUFF
+            //
+
+            // TODO
+
+            //
+            // RENDER STUFF
+            //
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+
+            // TODO
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
+
